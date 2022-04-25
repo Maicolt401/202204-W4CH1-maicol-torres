7e62fa87203ef5fdb2065c67002b929f2731f75e
maicol torres <maicolt401@gmail.com>
2022-04-25 19:57:09 +0200

    elaborate header on indes.js con rat

---
+++ src/gentlemen/button/Button/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ selectAll, action }) => {
+  return (
+    <section class="controls">
+      <p class="info">0 gentlemen pointing at you</p>
+      <button class="button button--select">Select all</button>
+    </section>
+  );
+};
+export default Button;
