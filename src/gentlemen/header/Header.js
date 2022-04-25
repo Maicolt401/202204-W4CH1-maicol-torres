e41b21a00a5587307bb099526326860434ac4ff4
maicol torres <maicolt401@gmail.com>
2022-04-25 22:56:24 +0200

    integrate indx whit comonents

---
+++ src/gentlemen/header/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+      <header className="main-header">
+        <h1 className="main-title">Señores que te apuntan con el dedo</h1>
+      </header>
+      <section className="controls"></section>
+      <main className="main">
+        <ul className="gentlemen"></ul>
+      </main>
+    </>
+  );
+};
+export default Header;
